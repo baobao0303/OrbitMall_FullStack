a19649ce53ccdbd2cfca6599866449b20b617b03
baobao22.work <baobao22.work@gmail.com>
2025-08-05 05:48:47 +0000

    update library view

--- OrbitMall_Portal/projects/view/base/src/public-api.ts
+++ OrbitMall_Portal/projects/view/base/src/public-api.ts
@@ -1,6 +1,19 @@
 /*
  * Public API Surface of base
  */
-
-export * from './lib/base.service';
-export * from './lib/base.component';
+export * from './lib/view.active';
+export * from './lib/view.active.registry';
+export * from './lib/view.aggregation.context';
+export * from './lib/view.base';
+export * from './lib/view.command';
+export * from './lib/view.command.base';
+export * from './lib/view.command.helper';
+export * from './lib/view.command.mediator';
+export * from './lib/view.command.registry';
+export * from './lib/view.context';
+export * from './lib/view.edit.context';
+export * from './lib/view.list.context';
+export * from './lib/view.options';
+export * from './lib/view.ref.mapper.registry';
+export * from './lib/view.render.registry';
+export * from './lib/view.type';
