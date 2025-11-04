254173ea1cf1b5bd5979d8fab1fcea8fed28f45f
Lê Tấn Bảo Bảo <110554959+baobao0303@users.noreply.github.com>
2025-11-04 10:12:45 +0700

    feature(auth-service): thêm auth service bên node sang go đổi db sang postgresql

---
+++ services/auth/cmd/api/main.go
@@ -0,0 +1,87 @@
+package main
+
+import (
+	"database/sql"
+	"fmt"
+	"log"
+	"net/http"
+	"os"
+	"ride-sharing/services/auth/data"
+	"time"
+
+	_ "github.com/jackc/pgx/v4/stdlib"
+)
+
+const webPort = "80"
+var counts int64
+
+type Config struct {
+	DB *sql.DB
+	Models data.Models
+}
+
+func main() {
+
+	//log the start of the application
+	log.Println("Starting authentication service")
+
+
+	// connect to database
+	db, err := connectToDB()
+	if err != nil {
+		log.Fatalf("Error connecting to database: %s\n", err)
+		os.Exit(1)
+	}
+
+	// setup config
+	app := Config{
+		DB: db,
+		Models: data.New(db),
+	}
+
+
+	src := &http.Server{
+		Addr: fmt.Sprintf(":%s", webPort),
+		Handler: app.routes(),
+	}
+
+	// start the server
+	err = src.ListenAndServe()
+	if err != nil {
+		log.Fatalf("Error starting server: %s\n", err)
+		os.Exit(1)
+	}
+}
+
+
+//configure database
+func openDB(dsn string) (*sql.DB, error) {
+	// if using env os.Getenv("DB_URL")
+	db, err := sql.Open("pgx", dsn )
+	if err != nil {
+		return nil, err
+	}
+
+	if err := db.Ping(); err != nil {
+		return nil, err
+	}
+
+	return db, nil
+}
+
+func connectToDB() (*sql.DB, error) {
+	dsn := os.Getenv("DSN")
+	for{
+		db, err := openDB(dsn)
+		if err != nil {
+			log.Println("Postgres not ready yet...")
+			counts++
+			if counts > 10 {
+				return nil, err
+			}
+			time.Sleep(2 * time.Second)
+			continue
+		}
+		return db, nil
+	}
+}
